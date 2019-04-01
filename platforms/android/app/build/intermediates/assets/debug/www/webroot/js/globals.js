3272ed3671579c7a4732c09f2db78c2a37b3525e
José Carlos Romero Vera <veracarlos66@hotmail.es>
2019-04-01 13:40:18 -0500

    Revert "Para compilar"
    This reverts commit 04a83ba2720c1b087efb38368c2fac954e1f558e.

--- platforms/android/app/build/intermediates/assets/debug/www/webroot/js/globals.js
+++
@@ -1,8 +0,0 @@
-// window.server = "http://localhost/drawapp/php/";
-// window.url = "http://localhost/drawapp/";
-
-// window.server = "http://192.168.1.79/drawapp/php/";
-// window.url = "http://192.168.1.79/drawapp/";
-
-window.server = "https://services.sm2consultores.com/drawapp/php/";
-window.url = "http://services.sm2consultores.com/drawapp/";
